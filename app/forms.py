a37d244d33a150d85c40f6db288aa4e6fbbef63d
Richard Bush <rjb2@princeton.edu>
2016-02-15 23:09:36 -0500

    another one

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,23 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, PasswordField, IntegerField, SelectField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
+    email    = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+
+
+class SignUpForm(Form):
+
+    email    = StringField('email', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+    conPass  = StringField('conPass', validators=[DataRequired()])
+    fullname = StringField('fullname', validators=[DataRequired()])
+    age      = IntegerField('age', validators = [DataRequired()])
+    
+    carType  = SelectField(u'Programming Language', choices=[('truck','Truck'),('sedan', 'Sedan'), ('SUV', 'SUV'), ('none', 'I don\'t have a car')])
+
+
--- app/views.py
+++ app/views.py
@@ -1,25 +1,43 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app import app
-from .forms import LoginForm
+from .forms import LoginForm, SignUpForm
 
+# Reroute to login screen
 @app.route('/', methods=['GET', 'POST'])
+def hello():
+    return redirect('/login')
+
+
+# Have user log in or redirect to sign up 
+@app.route('/login', methods=['GET', 'POST'])
 def welcome():
     # check if they logged in -- if they have call function that
     # shoes you next screen
     form = LoginForm()
-    
-
     # Name, Age, Email, Phone Number, Type of Car
-
     # Page with two buttons that say I am looking for a ride or I have a ride 
-
     return render_template('welcome.html', form=form)
- 
 
-@app.route('/giveorfind')
-def giveOrFind():
 
-    return "here we will present some text"
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+
+    form = SignUpForm()
+
+    if form.validate_on_submit():
+        # save data to database here 
+            return redirect('/login')
+
+    return render_template('signup.html', form = form)
+
+
+
+
+
+
+
+
+
 
 @app.route('/findrides')
 def find_rides():
@@ -40,6 +58,12 @@ def my_form_post():
     return render_template("results.html", dest = dest, orig = orig, persons = newPersons)
 
 
+
+ 
+@app.route('/giveorfind')
+def giveOrFind():
+
+    return "here we will present some text"
 if __name__ == '__main__':
     app.run(debug=True)
 
